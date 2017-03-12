d0c6c090514fbecf0256ea6035bea1f65cbea95f
wowteacher <francesca.sampietro@gmail.com>
2017-03-12 15:23:52 +0100

    converto testo in punti

--- sketch.js
+++ sketch.js
@@ -8,13 +8,23 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(800,300);
-  background(0);
-  textFont(font);
-  textSize(128);
-  fill(255);
-  noStroke();
-  text("Gina", 10, 200);
+  createCanvas(600,300);
+  background(45,48,71); //gunmetal
+  // textFont(font);
+  // textSize(150);
+  // fill(27, 153, 139); // jungle green
+  // noStroke();
+  // text("Gina", 100, 200);
+
+  //uso textToPoints per creare la scritta con i punti
+  var points = font.textToPoints ('Gina', 100, 200, 150);
+
+  for (var i = 0; i < points.length; i++){
+    var pt = points[i];
+    stroke(27, 153, 139); // jungle green
+    strokeWeight(8);
+    point(pt.x,pt.y);
+  }
 
 }
 
